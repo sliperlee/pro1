cb7f26afc0f8cad27353d4efd6969a4e03007cdf
lipeng <12802900960@qq.com>
2019-06-29 18:45:15 +0800

    把本地工程添加到git

---
+++ src/com/itheima/git/FirstClass.java
@@ -0,0 +1,20 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+
+    public void test(){
+        int i = 2;
+        i +=10;
+
+        System.out.println(i);
+    }
+}
